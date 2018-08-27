9bd8f2278bd8fb2c9e987452a1d0c953b04ac3e5
jose arjona <josedarjona@gmail.com>
2018-08-26 21:32:16 -0400

    top ans

--- vowel-count/vowel-count.js
+++ vowel-count/vowel-count.js
@@ -9,3 +9,27 @@ function getCount(str) {
   
   return vowelsCount;
 }
+
+
+//one line regex
+
+// function getCount(str) {
+//   return (str.match(/[aeiou]/ig)||[]).length;
+// }
+
+
+
+// //for loop, readability
+// function getCount(str) {
+//   var vowelsCount = 0;
+//   var vowels = ["a","e","i","o","u"];
+//   for(var i = 0;i < str.length;i++){
+//     for(var j=0;j<vowels.length;j++){
+//       if(str[i] === vowels[j]){
+//         vowelsCount++;
+//       }
+//     }
+//   }
+  
+//   return vowelsCount;
+// }
