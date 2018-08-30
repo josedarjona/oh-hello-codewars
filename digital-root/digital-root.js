da4a18c3b0dce8f241289b0aa8c3e9ae705fc840
jose arjona <josedarjona@gmail.com>
2018-08-29 23:48:38 -0400

    recursion

--- digital-root/digital-root.js
+++ digital-root/digital-root.js
@@ -27,3 +27,14 @@ function digital_root(n) {
 // function digital_root(n) {
 //   return (n - 1) % 9 + 1;
 // }
+
+
+//recursion
+
+
+// function digital_root(n) {
+//   if (n < 10) return n;
+  
+//   return digital_root(
+//     n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+// }
